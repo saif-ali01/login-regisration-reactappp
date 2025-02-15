de784fa3088c08a398424a3b997cec5c7b1071de
saif-ali01 <saif-ali01@outlook.com>
2025-02-15 13:07:08 +0530

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const navigate = useNavigate();
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true); 
 
   useEffect(() => {
@@ -12,6 +12,7 @@ const Home = () => {
       navigate("/login")
     } else {
       setUser(userData);
+      setLoading(false);
     }
   }, [navigate]);
 
@@ -20,37 +21,28 @@ const Home = () => {
     navigate("/login");
   };
 
-  const handleImageLoad = () => {
-    setLoading(false); 
-  };
+ 
 
   return (
-    <div
-      className="h-screen w-screen flex justify-center "
-      style={{
-        backgroundImage: `url(/img.png)`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
+    <div      className="h-screen w-screen flex justify-center "    >
       {loading ? (
         <div className=" w-screen h-screen flex justify-center items-center ">
           <div className="h-10 w-10  animate-spin border-t-2 rounded-full border-[#6247c5] border-x-4"></div>
         </div> 
       ) : (
         <div
-          className="font-serif px-4 bg-[rgba(0,0,0,0.5)] h-2/4 rounded-xl justify-center tracking-wider text-gray-400 flex flex-col items-center mt-[15%] text-center"
+          className="font-serif px-4 sm:bg-[rgba(255,255,255,0.1)] h-2/4 rounded-xl justify-center tracking-wider text-gray-400 flex flex-col items-center mt-[15%] text-center"
           style={{ fontFamily: "Montserrat" }}
         >
           <h5
-            className={ `uppercase bg-yellow-500 rounded-lg text-white px-2 font-bold text-[40px]`}
+            className={ `uppercase px-2 font-bold text-[40px]`}
             style={{ fontFamily: "Zen Dots", fontWeight: 900 }}
           >
             hey! {user?.name}
           </h5>
 
           <h5
-            className={` font-bold mt-[20px] text-[30px]`}
+            className={` font-bold mt-[20px] text-[20px] sm:text-[30px]`}
             style={{ fontFamily: "Zen Dots", fontWeight: 900 }}
           >
             You Successfully Logged into our System
@@ -64,13 +56,7 @@ const Home = () => {
         </div>
       )}
 
-      {/* Use onLoad to detect when the image is fully loaded */}
-      <img
-        src="/img.png"
-        alt="background"
-        onLoad={handleImageLoad}
-        style={{ display: "none" }} // Hide the image element itself
-      />
+   
     </div>
   );
 };
